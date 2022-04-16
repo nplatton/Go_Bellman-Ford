850383eafdf3bacc11a03f373abc59e160de12cd
Noah Platton <noahplatton@gmail.com>
2022-04-16 10:37:49 +0100

    build: fix fileReaders import

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module practice/go_bellman-ford
 go 1.18
 
 replace practice/fileReaders => ./fileReaders
+
+require practice/fileReaders v0.0.0-00010101000000-000000000000
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
-	F "./fileReaders"
+	F "practice/fileReaders"
 )
 
 func main() {
+
 	srcCities, dstCities := F.Get_Test_Data("test.json")
 	fmt.Println(srcCities, dstCities)
 	
+
 }
