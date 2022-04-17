a091990e51d8829b20b9ad8d5dbfdd4898c94cf9
Noah Platton <noahplatton@gmail.com>
2022-04-17 18:15:58 +0100

    Create helper fcts to find node name/id

---
+++ helpers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	Fr "practice/fileReaders"
+)
+
+func Find_Node_Ids(src_name, dst_name string) (srcID, dstID int) {
+	// Retrieve array of all n
+	nodes := Fr.Get_All_Nodes("data.txt")
+	// Initialise the IDs
+	srcID, dstID = -1, -1
+	// Calculate the node ids
+	for i:=0; i<len(nodes); i++ {
+		if nodes[i].name == src_name {
+			srcID = i
+		} else if nodes[i].name == dst_name {
+			dstID = i
+		}
+		if srcID!=-1 && dstID!=-1 {
+			break
+		}
+	}
+	return
+}
+
+func Find_Node_Name(node_id int) string {
+	// Retrieve array of all n
+	nodes := Fr.Get_All_Nodes("data.txt")
+	// Calculate the node ids
+	for i:=0; i<len(nodes); i++ {
+		if nodes[i].id == node_id {
+			return nodes[i].name
+		}
+	}
+	return ""
+}
